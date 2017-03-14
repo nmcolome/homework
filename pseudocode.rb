e2873719312f46a7c24dcb4dde2c26e48217b764
Natalia Colomé <nmcolome@gmail.com>
2017-03-14 11:58:47 -0600

    Update pseudocode.rb

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,25 +1,22 @@
-puts "Welcome"
-input = gets.chomp
-counter = 1
+Bad Connection
 
-until counter == 2 do
-        if input.downcase == "bye"
-            counter += 1
-            puts "anything else?"
-            input = gets.chomp
-        elsif input == ""
-            puts "HELLO"
-            counter = 0
-            input = gets.chomp
-        elsif input == input.downcase
-            puts "I cant hear you"
-            counter = 0
-            input = gets.chomp
-        elsif input == input.upcase
-            puts "its not a pet store"
-            counter = 0
-            input = gets.chomp
-        end
-end
+1. How will you know when the problem is solved? (Identify the big-picture goal)
+When the client writes "goodbye" for the second time and the program stops
+2. How do you want to use the software? (Identify the “interface”)
+Through prompts
+3. What’s the (next-)most trivial possible case? (Identify the next small-picture goal)
+Each answer of the client
+4. How do we achieve this goal? (Sketch an algorithm using pseudocode)
 
-puts "thank you for coming"
+	
+	1. Send a welcome message and prompt for an input
+	2. The user writes a message.
+	3. I evaluate the message. If:
+		a. Input = nothing => HELLO?! And ask for prompt.
+		b. Input = lower-case and "?" => I AM HAVING A HARD TIME HEARING YOU. And ask for prompt.
+		c. Input = upper-case and "?" => NO, THIS IS NOT A PET STORE. And ask for prompt.
+		d. Input = goodbye!
+			i. Turn to a determined case.
+			ii. Evaluate if it the first time or second time it sends it. (count)
+				1) If 1st time => ANYTHING ELSE I CAN HELP WITH? And ask for prompt.
+				2) If 2nd time => THANK YOU FOR CALLING! … and exit the program
