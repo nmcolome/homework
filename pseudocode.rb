70ce320c2d853648b9a19dc7d502f75acb90b846
NataliaColome <nmcolome@gmail.com>
2017-03-15 11:32:27 -0600

    pseudocode with code

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,3 +1,5 @@
+=begin
+pseudocode:
 Send a welcome message and prompt for an input
 The user writes a message.
 I evaluate the message. If:
@@ -9,3 +11,32 @@ I evaluate the message. If:
 			ii. Evaluate if it the first time or second time it sends it. (count)
 				1) If 1st time => ANYTHING ELSE I CAN HELP WITH? And ask for prompt.
 				2) If 2nd time => THANK YOU FOR CALLING! … and exit the program
+
+This didn't work in this order... I had to evaluate the "goodbye!" first, then the other conditions.
+TO REMEMBER: THE CONDITION DEPENDS ON "GOODBYE!" WHICH SHOULD BE THE FIRST THING I EVALUATE TO AVOID CONFUSION WITHIN MY CODE.
+=end
+
+require "byebug"
+
+puts "HELLO, THIS IS A GROCERY STORE!"
+input = gets.chomp
+answers = []
+answers.push (input.downcase)
+
+until answers.count("goodbye!")==2
+	if input.capitalize == "Goodbye!"
+		puts "ANYTHING ELSE I CAN HELP WITH?"
+	elsif input == ""
+		puts "HELLO?!"
+	elsif input == input.upcase
+		puts "NO, THIS IS NOT A PET STORE"
+	else input == input.downcase
+		puts "I AM HAVING A HARD TIME HEARING YOU."
+	end
+	input = gets.chomp
+	answers.push (input.downcase)
+end
+
+puts "THANK YOU FOR CALLING!"
+
+		 
