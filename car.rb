f1c8bdcfbcac2b8ac81017ab7dec63b28f53e6d2
NataliaColome <nmcolome@gmail.com>
2017-03-19 23:05:06 -0600

    homework update

---
+++ car.rb
@@ -0,0 +1,46 @@
+class Car
+    
+    attr_accessor :color, :wheel_count
+    
+    def initialize (start, color, wheel_count)
+        @start          = start
+        @color          = color
+        @wheel_count    = wheel_count
+    end
+
+    def horn
+        "BEEEEEP!"
+    end
+
+    def drive(distance)
+        "I'm driving #{distance} miles."
+    end
+    
+    def report_color
+        "I am #{color}"
+    end
+
+    def start
+        if start == false
+            puts "Starting up!"
+            start = true
+        else puts "BZZT! Nice try, though."
+        end
+    end
+
+end
+
+
+my_car = Car.new(false, "red", 18)
+puts my_car.horn
+puts my_car.drive(12)
+my_car.color = "purple"
+puts my_car.report_color
+my_car.wheel_count = 18
+puts "This sweet ride is sitting on #{my_car.wheel_count} wheels"
+
+my_second_car = Car.new(false,"red",18)
+my_second_car.wheel_count = 2
+puts "This sweet ride is sitting on #{my_second_car.wheel_count} wheels"
+
+my_car.start
--- credit_check.rb
+++ credit_check.rb
@@ -1,47 +1,35 @@
+puts "Enter card number:"
+card_number = gets.chomp
 
-card_number = "4929735477250543"
-
-valid = false
 total = card_number.length - 1
-empty = []
 
 if total.odd?
-    (0..total).to_a.each do |index|
-    if index.odd?
-        empty.push(card_number[index].to_i * 2)
-    else empty.push(card_number[index].to_i)
+    doubled_elements = (0..total).to_a.map do |index|
+        if index.even?
+            card_number[index].to_i * 2
+        else card_number[index].to_i
+        end
     end
-end
-else (0..total).to_a.each do |index|
-    if index.pair?
-        empty.push(card_number[index].to_i * 2)
-    else empty.push(card_number[index].to_i)
-    end
-end
-end
-
-# puts empty
-new_collection = []
-(0..total).to_a.each do |index|
-    if empty[index].to_s.length > 1
-        new_number = empty[index].to_s[0].to_i + empty[index].to_s[1].to_i
-        new_collection.push(new_number)
-    else new_collection.push(empty[index])
+    
+    else doubled_elements = (0..total).to_a.map do |index|
+        if index.odd?
+            card_number[index].to_i * 2
+        else card_number[index].to_i
+        end
     end
 end
 
-# puts new_collection
+new_collection = (0..total).to_a.map do |index|
+    if doubled_elements[index].to_s.length > 1
+        new_number = doubled_elements[index].to_s[0].to_i + doubled_elements[index].to_s[1].to_i
+    else doubled_elements[index]
+    end
+end
 
 sum = 0
 new_collection.each {|sum_number| sum += sum_number}
-# puts sum
 
-validation = (sum + (10 - sum.to_s[1].to_i))
-# puts validation
-
-if validation % 10 == 0
-    valid = true
+if sum % 10 == 0
     puts "The number is valid!"
 else puts "The number is invalid!"
 end
-
